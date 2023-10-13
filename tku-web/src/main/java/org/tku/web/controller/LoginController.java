5c7e0af49a2976e559ed20c8fcb2f1f6d3d5aa04
whitestorm2346 <andrew770426@gmail.com>
2023-10-13 16:12:54 +0800

    Added the common templates for the web app

--- tku-web/src/main/java/org/tku/web/controller/LoginController.java
+++ tku-web/src/main/java/org/tku/web/controller/LoginController.java
@@ -3,6 +3,7 @@ package org.tku.web.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.LocaleResolver;
 import java.util.Locale;
 
 @Controller
+@Log4j2
 public class LoginController {
     @Autowired
     LocaleResolver localeResolver;
@@ -26,7 +28,14 @@ public class LoginController {
     }
 
     @GetMapping(value = {"/login", "/"})
-    public String login() throws JsonProcessingException {
+    public String loginPage() throws JsonProcessingException {
         return "login";
+    }
+
+    @PostMapping(value = "/login")
+    public String login(String userName, String password) throws JsonProcessingException {
+        log.debug("userName = {}, password = {}", userName, password);
+
+        return "index";
     }
 }
