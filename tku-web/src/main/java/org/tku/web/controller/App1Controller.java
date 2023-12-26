e8731e917b5180e8297bfbca1ecadd5835e1c2f4
whitestorm2346 <andrew770426@gmail.com>
2023-12-26 20:33:31 +0800

    Redesigned the website using Thymeleaf, finished the index.html

--- tku-web/src/main/java/org/tku/web/controller/App1Controller.java
+++ tku-web/src/main/java/org/tku/web/controller/App1Controller.java
@@ -1,13 +1,14 @@
 package org.tku.web.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class App1Controller {
 
-    @GetMapping(value = "/web/app1")
-    public String app1() {
-        return "app1";
+    @GetMapping(value = "/note-share/notes")
+    public String NotesPage() throws JsonProcessingException {
+        return "notes";
     }
 }
--- tku-web/src/main/java/org/tku/web/controller/SignUpController.java
+++ tku-web/src/main/java/org/tku/web/controller/SignUpController.java
@@ -1,40 +1,31 @@
 package org.tku.web.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.LocaleResolver;
 
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
 @Controller
 @Log4j2
-public class LoginController {
+public class SignUpController {
     @Autowired
     LocaleResolver localeResolver;
 
-    @GetMapping(value = {"/index", "/"})
-    public String index(@RequestParam(required = false) String locale,
-                        HttpServletRequest request, HttpServletResponse response) {
-        if(StringUtils.isNotBlank(locale)) {
-            localeResolver.setLocale(request, response, new Locale(locale));
-        }
-        return "index";
-    }
-    @GetMapping(value = "/login")
+    @GetMapping(value = "/sign-up")
     public String loginPage() throws JsonProcessingException {
         return "login";
     }
 
-    @PostMapping(value = "/login")
+    @PostMapping(value = "/sign-up")
     public String login(String userName, String password) {
         log.debug("userName = {}, password = {}", userName, password);
         return "redirect:/index";
